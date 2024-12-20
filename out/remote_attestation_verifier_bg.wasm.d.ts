aa8c375d794c4cfbaaf86fceb162fcb80415539d
asa93 <asa93@live.fr>
2024-12-20 10:31:46 -0300

    add wasm bindings

---
+++ out/remote_attestation_verifier_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const verify_js: (a: number, b: number, c: number, d: number, e: number, f: number, g: any, h: number, i: number, j: bigint) => [number, number];
+export const ring_core_0_17_8_bn_mul_mont: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
